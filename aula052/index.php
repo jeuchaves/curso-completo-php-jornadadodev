52fc88ee7085fc240254bddb6cd0a9e1db012f71
Jeú Chaves Lima <jeuchaveslima@gmail.com>
2022-07-05 17:39:54 -0400

    Aula 052: Definindo, incluindo, alterando e excluindo elementos do array

---
+++ aula052/index.php
@@ -0,0 +1,13 @@
+<?php
+# Definindo o Array
+$arr = ["zero", "um", "dois", "tres"];
+
+# Adicionando elementos
+$arr[] = "quatro";
+
+# Alterando elementos
+$arr[1] = 4;
+$arr[4] = 4;
+
+# Excluindo elementos
+unset($arr[3]);
