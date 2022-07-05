c697d3f55ea62a8a3fe7e2d1866f66e6bbdf851c
Jeú Chaves Lima <jeuchaveslima@gmail.com>
2022-07-05 15:05:27 -0400

    Aula 016: Propriedade das variáveis

---
+++ aula016/index.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Propriedades das variáveis:
+ * Nome     => ex: $nome, $num...
+ * Tipo     => ex: string, int, char, double, boolean...
+ * Tamanho  => Espaço ocupado na memória
+ * Valor    => ex: 10, "texto", true, 10.5...
+ */
