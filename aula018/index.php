d3d910f6db260b525dfa0905aaf7b3c0e759e8f0
Jeú Chaves Lima <jeuchaveslima@gmail.com>
2022-07-05 15:10:48 -0400

    Aula 018: Operadores Aritméticos

---
+++ aula018/index.php
@@ -0,0 +1,14 @@
+<?php
+
+$num1 = 10;
+$num2 = 5;
+
+$soma = $num1 + $num2;
+$subtracao = $num1 - $num2;
+$multiplicacao = $num1 * $num2;
+$divisao = $num1 / $num2;
+
+echo 'Soma          : ' . $soma;
+echo '<br>Subtracao     : ' . $subtracao;
+echo '<br>Multiplicacao : ' . $multiplicacao;
+echo '<br>Divisao       : ' . $divisao;
