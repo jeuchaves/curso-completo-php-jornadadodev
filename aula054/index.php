9b563b91af684d31801fdfd0795efda159889f0d
Jeú Chaves Lima <jeuchaveslima@gmail.com>
2022-07-05 17:59:46 -0400

    Aula 054: Iterando Arrays

---
+++ aula054/index.php
@@ -0,0 +1,5 @@
+<?php
+$lista = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"];
+for($i = 0; $i < count($lista); $i++) {
+    echo 'Dia da semana: ' . $lista[$i] . '<br>'; 
+}
