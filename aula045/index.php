ba0bbc3958fd2571c1973382eaaa191a4bde65ec
Jeú Chaves Lima <jeuchaveslima@gmail.com>
2022-07-05 16:47:33 -0400

    Aula 045: Instrução Switch

---
+++ aula045/index.php
@@ -0,0 +1,17 @@
+<?php
+$num1 = 5;
+
+switch($num1) {
+    case 1:
+        echo 'Numero um';
+        break;
+    case 2:
+        echo 'Numero dois';
+        break;
+    case 3:
+        echo 'Numero tres';
+        break;
+    default:
+        echo 'Nao sei contar mais que isso';
+        break;
+}
